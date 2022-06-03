9c20d99255911f921eaf9a6080e712fa27fd8952
examples311 <examples311@yahoo.com>
2022-06-03 16:54:49 -0500

    2 files modified, 3 added

---
+++ src/app/my-logo/my-logo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-my-logo',
+  templateUrl: './my-logo.component.html',
+  styleUrls: ['./my-logo.component.css']
+})
+export class MyLogoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
